7250b28618af53ed70c5f3d305ab757a977bf6d7
Itai Spigelman <itai10020@gmail.com>
2021-01-06 21:08:14 +0200

    fix semicolon in class def, DateWrap start

--- partA/date_wrap.h
+++ partA/date_wrap.h
@@ -1,7 +1,14 @@
 namespace mtm {
-    
-    class DateWrap {
-
-    }
-
+    class DateWrap 
+    {
+        private:
+            int day=0;
+            int month=0;
+            int year=0;
+        public:
+            DateWrap(int day, int month, int year);
+            int& day();
+            int& month();
+            int& year();
+    };
 }
