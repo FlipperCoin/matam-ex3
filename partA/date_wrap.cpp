50132b9d2955029a6b0d85336285f6589c4ed824
Itai Spigelman <itai10020@gmail.com>
2021-01-06 22:02:44 +0200

    date wrap skeleton

--- partA/date_wrap.cpp
+++ partA/date_wrap.cpp
@@ -1,5 +1,52 @@
 #include "date_wrap.h"
 
 namespace mtm {
+    DateWrap::DateWrap(int day, int month, int year) {
     
+    }   
+
+    const int& DateWrap::day() const {
+        return this->m_day;
+    }
+
+    const int& DateWrap::month() const {
+        return this->m_month;
+    }
+
+    const int& DateWrap::year() const {
+        return this->m_year;
+    }
+
+    bool DateWrap::operator<=(const DateWrap& date) const {
+
+    }
+
+    bool DateWrap::operator>(const DateWrap& date) const {
+
+    }
+
+    bool DateWrap::operator>=(const DateWrap& date) const {
+
+    }
+
+    bool DateWrap::operator<(const DateWrap& date) const {
+
+    }
+
+    bool DateWrap::operator==(const DateWrap& date) const {
+    
+    }
+
+    bool DateWrap::operator!=(const DateWrap& date) const {
+        
+    }
+
+    DateWrap operator+(int days, const DateWrap& date) {
+        return date + days;
+    }
+
+    ostream& operator<<(ostream& in, const DateWrap& dateWrap) {
+
+    }
 }
+
--- partA/date_wrap.h
+++ partA/date_wrap.h
@@ -1,14 +1,35 @@
+#include <ostream>
+
+using std::ostream;
+
 namespace mtm {
     class DateWrap 
     {
         private:
-            int day=0;
-            int month=0;
-            int year=0;
+            int m_day=0;
+            int m_month=0;
+            int m_year=0;
         public:
             DateWrap(int day, int month, int year);
-            int& day();
-            int& month();
-            int& year();
+            DateWrap(DateWrap& dateWrap) = default;
+            ~DateWrap() = default;
+            DateWrap& operator=(const DateWrap& date) = default;
+            const int& day() const;
+            const int& month() const;
+            const int& year() const;
+
+            DateWrap operator++(int);
+            DateWrap operator+(int days) const;
+            DateWrap& operator+=(int days);
+
+            bool operator<(const DateWrap& date) const;
+            bool operator==(const DateWrap& date) const;
+            bool operator!=(const DateWrap& date) const;
+            bool operator<=(const DateWrap& date) const;
+            bool operator>(const DateWrap& date) const;
+            bool operator>=(const DateWrap& date) const;
+            friend ostream& operator<<(ostream& in, const DateWrap& dateWrap);
     };
+
+    DateWrap operator+(int days, const DateWrap& date);
 }
