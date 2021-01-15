544df6abc4023a79ba703f6f31f19c1a7992e0b0
Itai Spigelman <itai10020@gmail.com>
2021-01-15 18:48:17 +0200

    event container definition

--- partA/exceptions.h
+++ partA/exceptions.h
@@ -8,7 +8,12 @@ namespace mtm {
     // DateWrap
     class NegativeDays : public Exception {};
     class InvalidDate : public Exception {};
+
+    // Events
     class InvalidStudent : public Exception {};
     class AlreadyRegistered : public Exception {};
     class NotRegistered : public Exception {};
+    class EventAlreadyExists : public Exception {};
+    class EventDoesNotExist : public Exception {};
+    class RegistrationBlocked : public Exception {};
 }
--- partC/schedule.cpp
+++ partC/schedule.cpp
@@ -1,5 +1,33 @@
 #include "schedule.h"
 
 namespace mtm {
-    
+
+    void Schedule::addEvents(const EventContainer& container) {
+
+    }
+
+    void Schedule::registerToEvent(const DateWrap &eventDate, const std::string &eventName, int student) {
+
+    }
+
+    void Schedule::unregisterFromEvent(const DateWrap &eventDate, const std::string &eventName, int student) {
+
+    }
+
+    void Schedule::printAllEvents() {
+
+    }
+
+    void Schedule::printMonthEvents(int month, int year) {
+
+    }
+
+    template<class Predicate>
+    void Schedule::printSomeEvents(Predicate predicate, bool verbose) {
+
+    }
+
+    void Schedule::printEventDetails(const DateWrap &eventDate, const std::string &eventName) {
+
+    }
 }
